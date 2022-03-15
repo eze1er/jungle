2e765acdfb93a4551ab4e5142c1ef9ac9e94adf3
eze1er <papaezechiel@gmail.com>
2022-03-15 21:21:27 +0000

    Credentials spec ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, length: { minimum: 3 }, presence: true
+  validates :password_confirmation, length: { minimum: 3 }, presence: true
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
+
+  # def validate(input)
+  #   validates :user, presence: true,
+  #   if validate[:users].any? { |user| input.send(user) == "Evil" }
+  #   input.errors.add :user, "Credential false"
+  #   end
+  # end
+
+  def authenticate_with_credentials()
+    user = User.find_by(username: params['username'])
+    if user && user.authenticate(params['password'])
+      toketoken = JWT.encode({user_id: user.id}, 'mykey', 'HS256')
+      render json: { user: user, token: token }
+    else 
+      render json: { error: 'invalid credentials' }, status: 401
+    end 
+  end 
 end
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
-require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Validations' do
+    # validation examples here
+    it "is valid with valid attributes" do
+      @user = User.create(
+        first_name: "Ezechiel", 
+        last_name: "Iti", 
+        email: "a@aol.com", 
+        password: "1234567", 
+        password_confirmation: "1234567"
+      )
+      expect(@user).to be_valid
+    end 
+  end
+
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+    it "should only create user with valid passwords" do
+      @user = User.create(first_name: "Ezechiel", last_name: "Iti", email: "a@aol.com", password: "1234567", password_confirmation: "1234567")
+      expect(@user).to be_valid
+    end
+  end
 end
+
